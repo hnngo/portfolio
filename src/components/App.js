b38c00b622cf180bf863d77eed380e70f0f8e6e8
Nhan <hnngo@ntu.edu.sg>
2018-10-09 20:42:52 +0800

    Init project

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1><i className="icon ion-md-heart"></i>Nhan Ngo<i className="icon ion-md-heart"></i></h1>
+                <h4>Research Engineer/ Front-end developer</h4>
+                <div>
+                    <button>Home</button>
+                    <button>About</button>
+                    <button>My Resume</button>
+                    <button>Contact</button>
+                </div>
+                <h4>Hello,</h4>
+                <p>A skilled, knowledgeable and qualified student with solid academic background in
+                    electrical and electronics engineering who has strong knowledge in Telecommunication
+                    Engineering programing. Dedicated professional with expertise in Network Analysis,
+                    Image Processing, Antenna Design, and Digital Signal Processing. Possesses excellent
+                    time management and organization skills.</p>
+
+
+            </div>
+        );
+    }
+}
+
+export default App;
