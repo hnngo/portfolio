c20dcdbaa2f09ff0242c2841477d000afb47bf07
Nhan <hnngo@ntu.edu.sg>
2018-10-24 17:39:08 +0800

    Updates

---
+++ precache-manifest.fcfa3dc053d59154aefa72852e9a5db9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b3cf24e6f57e699c913204f53409e2a",
+    "url": "/portfolio/static/media/profile-photo.8b3cf24e.jpg"
+  },
+  {
+    "revision": "c982db437fa9cc4483af",
+    "url": "/portfolio/static/css/main.151fd9be.chunk.css"
+  },
+  {
+    "revision": "96fe70f83616d4d908aa",
+    "url": "/portfolio/static/js/1.96fe70f8.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a4881855fd2de1bd8516d497e6d26baf",
+    "url": "/portfolio/static/media/background_header.a4881855.jpeg"
+  },
+  {
+    "revision": "c982db437fa9cc4483af",
+    "url": "/portfolio/static/js/main.c982db43.chunk.js"
+  },
+  {
+    "revision": "34f4ab454d138f6ce5f6e7a2a5389490",
+    "url": "/portfolio/static/media/project-1-a.34f4ab45.jpg"
+  },
+  {
+    "revision": "d785eb65bd90a68f82d03fdadb95e336",
+    "url": "/portfolio/static/media/project-1-b.d785eb65.JPG"
+  },
+  {
+    "revision": "c720bafda128f6b2f102fb3672cd6cb5",
+    "url": "/portfolio/static/media/project-1-c.c720bafd.jpg"
+  },
+  {
+    "revision": "cc1aad39caa299171dff291050f85832",
+    "url": "/portfolio/static/media/project-1-d.cc1aad39.JPG"
+  },
+  {
+    "revision": "3ed169fdc6de8d8a27d75a27b308df05",
+    "url": "/portfolio/index.html"
+  }
+];
