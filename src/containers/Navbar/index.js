230e71e44c4964d8ee7bd95a040bae7a3a06c123
Nhan <ngohuynhnhan@gmail.com>
2020-03-13 22:42:07 +0800

    Separate the render for Small and Big Nav

--- src/containers/Navbar/index.js
+++ src/containers/Navbar/index.js
@@ -3,6 +3,93 @@ import cx from "classnames";
 
 import styles from "./style.module.scss";
 
+const BigNav = ({ onClickSection }) => {
+  return (
+    <div className="d-none d-md-block">
+      <ul className={styles.bigNavUL}>
+        <li
+          className={styles.bigNavList}
+          onClick={() => onClickSection("about")}
+        >
+          <h6 className={styles.bigNavListText}>About</h6>
+          <div className={styles.bigNavUnderline} />
+        </li>
+        <li
+          className={styles.bigNavList}
+          onClick={() => onClickSection("experience")}
+        >
+          <h6 className={styles.bigNavListText}>Experience</h6>
+          <div className={styles.bigNavUnderline} />
+        </li>
+        <li
+          className={styles.bigNavList}
+          onClick={() => onClickSection("project")}
+        >
+          <h6 className={styles.bigNavListText}>Projects</h6>
+          <div className={styles.bigNavUnderline} />
+        </li>
+        <li
+          className={styles.bigNavList}
+          onClick={() => onClickSection("contact")}
+        >
+          <h6 className={styles.bigNavListText}>Contact</h6>
+          <div className={styles.bigNavUnderline} />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const SmallNav = ({ show, onClickSection, onClickExpand, onClickCollapse }) => {
+  const qNavCon = document.querySelector("#navbar");
+
+  return (
+    <div>
+      <i
+        className={cx(styles.smallNavIcon, "fas fa-bars d-sm-block d-md-none")}
+        onClick={() => {
+          show ? onClickCollapse() : onClickExpand();
+        }}
+      />
+      {!show ? null : (
+        <div
+          id="smallNavMenu"
+          className={styles.smallNavMenu}
+          style={{
+            top: qNavCon.clientHeight,
+            animation: "navSlideDown 0.6s"
+          }}
+        >
+          <div
+            className={styles.smallNavList}
+            onClick={() => onClickSection("about")}
+          >
+            <h6 className={styles.smallNavText}>About</h6>
+          </div>
+          <div
+            className={styles.smallNavList}
+            onClick={() => onClickSection("experience")}
+          >
+            <h6 className={styles.smallNavText}>Experience</h6>
+          </div>
+          <div
+            className={styles.smallNavList}
+            onClick={() => onClickSection("project")}
+          >
+            <h6 className={styles.smallNavText}>Projects</h6>
+          </div>
+          <div
+            className={styles.smallNavList}
+            onClick={() => onClickSection("contact")}
+          >
+            <h6 className={styles.smallNavText}>Contact</h6>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default class NavBar extends Component {
   constructor(props) {
     super(props);
@@ -13,35 +100,27 @@ export default class NavBar extends Component {
     };
   }
 
-  collapseNavMenu() {
+  collapseSmallNav() {
     // Collapse menu when state is opening
     const qSmMenu = document.querySelector("#smallNavMenu");
 
     if (qSmMenu) {
       qSmMenu.style.animation = "navSlideUp 1s";
-    }
 
-    setTimeout(() => {
-      this.setState({ showSmallMenu: false });
-    }, 1000);
-  }
-
-  handleClickSmMenuIcon() {
-    // If open, then collapse and open vice versa
-    if (this.state.showSmallMenu) {
-      this.collapseNavMenu();
-    } else {
-      this.setState({ showSmallMenu: true });
+      setTimeout(() => {
+        this.setState({ showSmallMenu: false });
+      }, 1000);
     }
   }
 
   handleClickNav(type, queryString = undefined) {
+    console.log(type);
     if (!type && !queryString) {
       return;
     }
 
     // Collapse the menu first
-    this.collapseNavMenu();
+    this.collapseSmallNav();
 
     // Get the right target
     let qTarget;
@@ -62,102 +141,6 @@ export default class NavBar extends Component {
     }
   }
 
-  renderSmallMenu() {
-    if (this.state.showSmallMenu) {
-      const qNavCon = document.querySelector("#navbar");
-
-      return (
-        <div
-          id="smallNavMenu"
-          className={styles.smallNavMenu}
-          style={{
-            top: qNavCon.clientHeight,
-            animation: "navSlideDown 0.6s"
-          }}
-        >
-          <div
-            className={styles.smallNavList}
-            onClick={() => this.handleClickNav("about")}
-          >
-            <h6 className={styles.smallNavText}>About</h6>
-          </div>
-          <div
-            className={styles.smallNavList}
-            onClick={() => this.handleClickNav("experience")}
-          >
-            <h6 className={styles.smallNavText}>Experience</h6>
-          </div>
-          <div
-            className={styles.smallNavList}
-            onClick={() => this.handleClickNav("project")}
-          >
-            <h6 className={styles.smallNavText}>Projects</h6>
-          </div>
-          <div
-            className={styles.smallNavList}
-            onClick={() => this.handleClickNav("contact")}
-          >
-            <h6 className={styles.smallNavText}>Contact</h6>
-          </div>
-        </div>
-      );
-    } else {
-      return <div />;
-    }
-  }
-
-  renderSmallNav() {
-    return (
-      <div>
-        <i
-          className={cx(
-            styles.smallNavIcon,
-            "fas fa-bars d-sm-block d-md-none"
-          )}
-          onClick={() => this.handleClickSmMenuIcon()}
-        />
-        {this.renderSmallMenu()}
-      </div>
-    );
-  }
-
-  renderBigNav() {
-    return (
-      <div className="d-none d-md-block">
-        <ul className={styles.bigNavUL}>
-          <li
-            className={styles.bigNavList}
-            onClick={() => this.handleClickNav("about")}
-          >
-            <h6 className={styles.bigNavListText}>About</h6>
-            <div className={styles.bigNavUnderline} />
-          </li>
-          <li
-            className={styles.bigNavList}
-            onClick={() => this.handleClickNav("experience")}
-          >
-            <h6 className={styles.bigNavListText}>Experience</h6>
-            <div className={styles.bigNavUnderline} />
-          </li>
-          <li
-            className={styles.bigNavList}
-            onClick={() => this.handleClickNav("project")}
-          >
-            <h6 className={styles.bigNavListText}>Projects</h6>
-            <div className={styles.bigNavUnderline} />
-          </li>
-          <li
-            className={styles.bigNavList}
-            onClick={() => this.handleClickNav("contact")}
-          >
-            <h6 className={styles.bigNavListText}>Contact</h6>
-            <div className={styles.bigNavUnderline} />
-          </li>
-        </ul>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div id="navbar" className={styles.container}>
@@ -166,12 +149,15 @@ export default class NavBar extends Component {
             <h6 className={styles.nText}>N</h6>
             <h6 className={styles.hText}>h</h6>
           </div>
-          {this.renderSmallNav()}
-          {this.renderBigNav()}
+          <SmallNav
+            show={this.state.showSmallMenu}
+            onClickSection={type => this.handleClickNav(type)}
+            onClickExpand={() => this.setState({ showSmallMenu: true })}
+            onClickCollapse={() => this.collapseSmallNav()}
+          />
+          <BigNav onClickSection={type => this.handleClickNav(type)} />
         </div>
       </div>
     );
   }
 }
-
-// Click small nav then collapse
