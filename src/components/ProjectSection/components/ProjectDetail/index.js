e6998fd02c0ab3cfac7fdf5e0831a2b13f40d039
Nhan <ngohuynhnhan@gmail.com>
2020-03-24 00:01:51 +0800

    Separate Project Detail and separate styles

---
+++ src/components/ProjectSection/components/ProjectDetail/index.js
@@ -0,0 +1,139 @@
+import React, { useRef } from "react";
+
+// Constants and utils
+import cx from "classnames";
+import { PROJECT_INFO, TECH_LOGO } from "../../../../data";
+import ReactTooltip from "react-tooltip";
+
+import styles from "./style.module.scss";
+
+const ProjectDetail = ({
+  animation,
+  selectedIndex,
+  onClickThumbnail,
+  onClickBackToBoardView,
+  slideLeftAnimation,
+  slideRightAnimation
+}) => {
+  const renderProjectDetail = () => {
+    if (selectedIndex !== undefined) {
+      let board = PROJECT_INFO[selectedIndex];
+
+      return (
+        <div className={styles.detailContainer}>
+          <ReactTooltip />
+          <div className="row">
+            <div className={`col-md-6 animated ${slideLeftAnimation} fast`}>
+              <div className="d-flex justify-content-between">
+                <p className={styles.detailTitle}>{board.name}</p>
+                <div className={styles.detailView}>
+                  {board.website.length > 0 && (
+                    <a
+                      className={styles.viewLink}
+                      href={board.website}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fas fa-external-link-alt" />
+                      <p>View Online</p>
+                    </a>
+                  )}
+                </div>
+              </div>
+              <p className={styles.detailDescription}>{board.description}</p>
+              <ul className={styles.detailList}>
+                {board.details.map((item, i) => (
+                  <li key={i}>{item}</li>
+                ))}
+              </ul>
+              <p className={styles.detailTech}>Technologies/Libraries</p>
+              {board.technologies.map((item, i) => {
+                // const imgRef = useRef();
+                return (
+                  <img
+                    // ref={imgRef}
+                    key={i}
+                    className={styles.detailTechLogo}
+                    src={TECH_LOGO[item]}
+                    alt="techLogo"
+                    data-tip={item}
+                    // onMouseEnter={() => ReactTooltip.show(imgRef.current)}
+                    // onMouseLeave={() => ReactTooltip.hide(imgRef.current)}
+                  />
+                );
+              })}
+              <div className={styles.detailView}>
+                {board.website.length > 0 && (
+                  <a
+                    className={styles.viewLink}
+                    href={board.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fas fa-external-link-alt" />
+                    <p>View Online</p>
+                  </a>
+                )}
+                {board.githubLink.length > 0 && (
+                  <a
+                    className={styles.viewLink}
+                    href={board.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-github" />
+                    <p>View Code</p>
+                  </a>
+                )}
+              </div>
+            </div>
+            <div className={`col-md-6 animated ${slideRightAnimation} fast`}>
+              <div className={styles.detailDevices}>
+                <img src={board.devices} alt={"imac-content"} />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  };
+
+  return (
+    <div>
+      <div className={styles.metaContainer}>
+        {/* Project Thumbnail Image Slider */}
+        {PROJECT_INFO.map((item, i) => (
+          <div
+            className={cx(styles.thumbnailSlider, `animated ${animation} fast`)}
+            key={i}
+          >
+            <img
+              className={cx(
+                styles.thumbnailImage,
+                i === selectedIndex && styles.selectedImage
+              )}
+              src={item.bannerImg}
+              alt="projects-banner"
+              onClick={() => onClickThumbnail(i)}
+            />
+          </div>
+        ))}
+        <div
+          className={cx(styles.backToBoardButton, `animated ${animation} fast`)}
+          onClick={onClickBackToBoardView}
+        >
+          <i className="fas fa-arrow-left" />
+          <p>Back to boards</p>
+        </div>
+      </div>
+      {renderProjectDetail()}
+      {/* <ReactTooltip /> */}
+    </div>
+  );
+};
+
+export default ProjectDetail;
+
+// TODO: Fix Tootltip
--- src/components/ProjectSection/index.js
+++ src/components/ProjectSection/index.js
@@ -3,10 +3,9 @@ import React, { Component } from "react";
 // Components
 import SectionTitle from "../../shared/SectionTitle";
 import ProjectCards from "./components/ProjectCards";
+import ProjectDetail from "./components/ProjectDetail";
 
 // Utils and constants
-import ReactTooltip from "react-tooltip";
-import { PROJECT_INFO, TECH_LOGO } from "../../data";
 import { SECTIONS_ID } from "../../shared/constants";
 
 import styles from "./style.module.scss";
@@ -16,7 +15,6 @@ export default class Projects extends Component {
     super(props);
 
     this.state = {
-      imgToggle: false,
       boardView: true,
       boardSelect: undefined,
       boardAnimation: "fadeIn",
@@ -24,13 +22,6 @@ export default class Projects extends Component {
       slideLeftAnimation: "slideInLeft",
       slideRightAnimation: "slideInRight"
     };
-  }
-
-  handleloadImg() {
-    // Handle if latency in loading img
-    this.setState({
-      imgToggle: !this.state.imgToggle
-    });
   }
 
   handleClickBoard(index, event) {
@@ -99,144 +90,10 @@ export default class Projects extends Component {
     );
   }
 
-  renderChangeViewBtn() {
-    return (
-      <div
-        className={`p-pt-vc animated ${this.state.thumbnailsAnimation} fast`}
-        onClick={() => this.handleClickBackToBoards()}
-      >
-        <i className="fas fa-arrow-left" />
-        <p>Back to boards</p>
-      </div>
-    );
-  }
-
-  renderProjectThumbnails() {
-    return PROJECT_INFO.map((item, i) => {
-      let style = {
-        filter: "brightness(50%) blur(0.7px)",
-        opacity: 0.5
-      };
-
-      // If project is selected then highlight it
-      if (i === this.state.boardSelect) {
-        style = {
-          filter: "brightness(100%) blur(0px)",
-          opacity: 1
-        };
-      }
-
-      return (
-        <div
-          className={`p-pt-ic animated ${this.state.thumbnailsAnimation} fast`}
-          key={i}
-        >
-          <img
-            src={item.bannerImg}
-            onLoad={() => this.handleloadImg()}
-            alt="projects-banner"
-            style={style}
-            onClick={() => this.handleClickThumbnail(i)}
-          />
-        </div>
-      );
-    });
-  }
-
-  renderProjectDetail() {
-    if (this.state.boardSelect !== undefined) {
-      let board = PROJECT_INFO[this.state.boardSelect];
-
-      return (
-        <div className="pd-container">
-          <div className="row">
-            <div
-              className={`col-md-6 animated ${this.state.slideLeftAnimation} fast`}
-            >
-              <div className="d-flex justify-content-between">
-                <p className="pd-name">{board.name}</p>
-                <div className="pd-view">
-                  {board.website.length > 0 ? (
-                    <a
-                      href={board.website}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fas fa-external-link-alt" />
-                      <p>View Online</p>
-                    </a>
-                  ) : (
-                    <div />
-                  )}
-                </div>
-              </div>
-              <p className="pd-des">{board.description}</p>
-              <ul className="pd-detail">
-                {board.details.map((item, i) => (
-                  <li key={i}>{item}</li>
-                ))}
-              </ul>
-              <p className="pd-tech">Technologies/Libraries</p>
-              {board.technologies.map((item, i) => {
-                return (
-                  <img
-                    ref={ref => (this["imgRef" + i] = ref)}
-                    key={i}
-                    className="pd-techLogo"
-                    src={TECH_LOGO[item]}
-                    alt="techLogo"
-                    data-tip={item}
-                    onMouseEnter={() => ReactTooltip.show(this["imgRef" + i])}
-                    onMouseLeave={() => ReactTooltip.hide(this["imgRef" + i])}
-                  />
-                );
-              })}
-              <div className="pd-view">
-                {board.website.length > 0 ? (
-                  <a
-                    href={board.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fas fa-external-link-alt" />
-                    <p>View Online</p>
-                  </a>
-                ) : (
-                  <div />
-                )}
-                {board.githubLink.length > 0 ? (
-                  <a
-                    href={board.githubLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fab fa-github" />
-                    <p>View Code</p>
-                  </a>
-                ) : (
-                  <div />
-                )}
-              </div>
-            </div>
-            <div
-              className={`col-md-6 animated ${this.state.slideRightAnimation} fast`}
-            >
-              <div className="pd-devices">
-                <img src={board.devices} alt={"imac-content"} />
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return <div />;
-    }
-  }
-
   render() {
     return (
-      <div id={SECTIONS_ID.PROJECTS} className="p-container">
-        {this.props.show ? (
+      <div id={SECTIONS_ID.PROJECTS} className={styles.container}>
+        {this.props.show && (
           <div className="container">
             <SectionTitle title="Projects" icon="fab fa-stumbleupon-circle" />
             {this.state.boardView ? (
@@ -245,18 +102,20 @@ export default class Projects extends Component {
                 onClickBoard={(i, e) => this.handleClickBoard(i, e)}
               />
             ) : (
-              <div>
-                <div className="p-pt-container">
-                  {this.renderProjectThumbnails()}
-                  {this.renderChangeViewBtn()}
-                </div>
-                {this.renderProjectDetail()}
-              </div>
+              <ProjectDetail
+                animation={this.state.thumbnailsAnimation}
+                selectedIndex={this.state.boardSelect}
+                onClickThumbnail={i => this.handleClickThumbnail(i)}
+                onClickBackToBoardView={() => this.handleClickBackToBoards()}
+                slideLeftAnimation={this.state.slideLeftAnimation}
+                slideRightAnimation={this.state.slideRightAnimation}
+              />
             )}
-            <ReactTooltip />
           </div>
-        ) : null}
+        )}
       </div>
     );
   }
 }
+
+// TODO: Fix delay in clicking thumbnail image
