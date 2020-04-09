cbb1c4b4f20d5d38dbc0cc30b2f531afca60ffc9
Nhan <ngohuynhnhan@gmail.com>
2020-04-09 11:11:10 +0800

    Update contact section with text centralize

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,57 +1,54 @@
 import React, { Component } from "react";
+
+// Constants
 import { SECTIONS_ID } from "../../shared/constants";
+import { CONTACT_DATA } from "../../data";
 
 import styles from "./style.module.scss";
 
 export default class ContactSection extends Component {
-  renderContent() {
-    return (
-      <div className="container animated fadeIn slow">
-        <div
-          className={styles.scrollToTopButton}
-          onClick={() =>
-            window.scroll({
-              top: 0,
-              behavior: "smooth"
-            })
-          }
-        >
-          <i className="fas fa-chevron-up" />
-        </div>
-        <div className={styles.socialSection}>
-          <a
-            href="https://github.com/hnngo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/ngohuynhnhan/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-linkedin-in" />
-          </a>
-          <a href="mailto:ngohuynhnhan@gmail.com">
-            <i className="fas fa-envelope-open-text" />
-          </a>
-        </div>
-        <div className={styles.logo}>
-          <div className={styles.titleLogo}>
-            <h6 className={styles.nText}>N</h6>
-            <h6 className={styles.hText}>h</h6>
-          </div>
-        </div>
-        <p className={styles.updateTime}>Last update 18 June 2019</p>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div id={SECTIONS_ID.CONTACT} className={styles.container}>
-        {this.renderContent()}
+        <div className="container animated fadeIn slow">
+          <div
+            className={styles.scrollToTopButton}
+            onClick={() =>
+              window.scroll({
+                top: 0,
+                behavior: "smooth",
+              })
+            }
+          >
+            <i className="fas fa-chevron-up" />
+          </div>
+          <div className={styles.socialSection}>
+            <a
+              href={CONTACT_DATA.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github" />
+            </a>
+            <a
+              href={CONTACT_DATA.linkedIn}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin-in" />
+            </a>
+            <a href={`mailto:${CONTACT_DATA.email}`}>
+              <i className="fas fa-envelope-open-text" />
+            </a>
+          </div>
+          <div className={styles.logo}>
+            <div className={styles.titleLogo}>
+              <h6 className={styles.nText}>N</h6>
+              <h6 className={styles.hText}>h</h6>
+            </div>
+          </div>
+          <p className={styles.updateTime}>Last update 11 April 2020</p>
+        </div>
       </div>
     );
   }
