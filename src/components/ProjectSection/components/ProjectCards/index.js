982f7e11e06e51ccafd6b8efa3e2d9f0283c9725
Nhan <ngohuynhnhan@gmail.com>
2020-04-11 13:21:48 +0800

    Add lazy image for project thumbnail image

--- src/components/ProjectSection/components/ProjectCards/index.js
+++ src/components/ProjectSection/components/ProjectCards/index.js
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+// Component
+import LazyImage from "../../../../shared/LazyImage";
+import porjectThumbnailPlaceholder from "../../../../style/img/project_thumbnail_ph.png";
+
 // Utils and constants
 import cx from "classnames";
 import { PROJECT_INFO } from "../../../../data";
@@ -7,19 +11,15 @@ import { PROJECT_INFO } from "../../../../data";
 import styles from "./style.module.scss";
 
 const ProjectCards = ({ animation, onClickBoard }) => {
-  const [imgToggle, setImgToggle] = React.useState(false);
-
   return (
     <div className={cx(styles.container, "row")}>
       {PROJECT_INFO.map((item, i) => (
         <div key={i} className={`col-md-4 col-sm-6 animated ${animation} fast`}>
           <div className={styles.cardContainer} onClick={() => onClickBoard(i)}>
-            <img
+            <LazyImage
               className={styles.projectThumbnail}
               src={item.bannerImg}
-              alt="projects-banner"
-              data-toggle={imgToggle}
-              onLoad={() => setImgToggle(!imgToggle)}
+              placeholder={porjectThumbnailPlaceholder}
             />
             <p className={styles.projectTitle}>{item.name}</p>
             <p className={styles.projectDescriptrion}>{item.description}</p>
--- src/shared/LazyImage/index.js
+++ src/shared/LazyImage/index.js
@@ -4,17 +4,20 @@ import cx from "classnames";
 import styles from "./style.module.scss";
 
 function LazyImage({ src, alt, placeholder, className, ...rest }) {
-  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [isLoaded, setIsLoaded] = React.useState(null);
 
   return (
     <div>
-      <img
-        className={cx(className, !isLoaded && styles.invisible)}
-        src={src}
-        alt={alt}
-        onLoad={() => setIsLoaded(true)}
-        {...rest}
-      />
+      {(isLoaded === null || isLoaded === true) && (
+        <img
+          className={cx(className, !isLoaded && styles.invisible)}
+          src={src}
+          alt={alt}
+          onLoad={() => setIsLoaded(true)}
+          onError={() => setIsLoaded(false)}
+          {...rest}
+        />
+      )}
       {!isLoaded && (
         <img className={className} src={placeholder} alt={alt} {...rest} />
       )}
