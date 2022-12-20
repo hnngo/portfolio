3ec05cc47ac81261602a28339ab9677f221458bb
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2022-12-19 23:44:57 -0800

    Update About section with S3 images and fix leaf components

--- src/components/IntroductionSection/index.js
+++ src/components/IntroductionSection/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import IntroductionTitle from "./components/IntroductionTitle";
 import IntroductionButton from "./components/IntroductionButton";
 import { SECTIONS_ID } from "../../shared/constants";
